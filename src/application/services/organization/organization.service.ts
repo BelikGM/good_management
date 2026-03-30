1ac79a57c77810975fd28d237d0d589d999840a5
valeriy <viaya.lysenko@mail.ru>
2026-03-30 16:27:09 +0300

    added new controller (findAllHighPost) and service (findAllWithHighPostForAccount)

--- src/application/services/organization/organization.service.ts
+++ src/application/services/organization/organization.service.ts
@@ -1,172 +1,213 @@
 import {
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
+    Inject,
+    Injectable,
+    InternalServerErrorException,
+    NotFoundException,
 } from '@nestjs/common';
-import { OrganizationRepository } from './repository/organization.repository';
-import { OrganizationReadDto } from 'src/contracts/organization/read-organization.dto';
-import { OrganizationCreateDto } from 'src/contracts/organization/create-organization.dto';
-import { Organization } from 'src/domains/organization.entity';
-import { AccountReadDto } from 'src/contracts/account/read-account.dto';
-import { OrganizationUpdateDto } from 'src/contracts/organization/update-organization.dto';
-import { Logger } from 'winston';
+import {DataSource, In} from 'typeorm';
+import {OrganizationRepository} from './repository/organization.repository';
+import {OrganizationReadDto} from 'src/contracts/organization/read-organization.dto';
+import {OrganizationCreateDto} from 'src/contracts/organization/create-organization.dto';
+import {Organization} from 'src/domains/organization.entity';
+import {OrganizationUpdateDto} from 'src/contracts/organization/update-organization.dto';
+import {Logger} from 'winston';
+import { Post } from 'src/domains/post.entity';
 
 @Injectable()
 export class OrganizationService {
-  constructor(
-    private readonly organizationRepository: OrganizationRepository,
-    @Inject('winston') private readonly logger: Logger,
-  ) {}
-
-  async findAllForAccount(
-    accountId: string,
-    relations?: string[],
-  ): Promise<OrganizationReadDto[]> {
-    try {
-      const organizations = await this.organizationRepository.find({
-        where: { account: { id: accountId } },
-        relations: relations ?? [],
-      });
-      return organizations.map((organization) => ({
-        id: organization.id,
-        organizationName: organization.organizationName,
-        parentOrganizationId: organization.parentOrganizationId,
-        reportDay: organization.reportDay,
-        colorCodes: organization.colorCodes,
-        organizationColor: organization.organizationColor,
-        createdAt: organization.createdAt,
-        updatedAt: organization.updatedAt,
-        users: organization.users,
-        posts: organization.posts,
-        goal: organization.goal,
-        policies: organization.policies,
-        projects: organization.projects,
-        strategies: organization.strategies,
-        controlPanels: organization.controlPanels,
-        account: organization.account,
-      }));
-    } catch (err) {
-      this.logger.error(err);
-      throw new InternalServerErrorException(
-        'Ошибка при получении всех организаций!',
-      );
+    constructor(
+        private readonly organizationRepository: OrganizationRepository,
+        private readonly dataSource: DataSource,
+        @Inject('winston') private readonly logger: Logger,
+    ) {
     }
-  }
 
-  async findOneById(
-    id: string,
-    relations?: string[],
-  ): Promise<OrganizationReadDto> {
-    try {
-      const organization = await this.organizationRepository.findOne({
-        where: { id: id },
-        relations: relations ?? [],
-      });
+    async findAllWithHighPostForAccount(accountId: string) {
+        const organizations = await this.organizationRepository.find({
+            where: { account: { id: accountId } },
+        });
+        const orgIds = organizations.map(org => org.id);
 
-      if (!organization)
-        throw new NotFoundException(`Организация с ID: ${id} не найдена`);
-      const organizationReadDto: OrganizationReadDto = {
-        id: organization.id,
-        organizationName: organization.organizationName,
-        parentOrganizationId: organization.parentOrganizationId,
-        reportDay: organization.reportDay,
-        colorCodes: organization.colorCodes,
-        organizationColor: organization.organizationColor,
-        createdAt: organization.createdAt,
-        updatedAt: organization.updatedAt,
-        users: organization.users,
-        posts: organization.posts,
-        goal: organization.goal,
-        policies: organization.policies,
-        projects: organization.projects,
-        strategies: organization.strategies,
-        controlPanels: organization.controlPanels,
-        account: organization.account,
-      };
+        const posts = await this.dataSource.getRepository(Post).find({
+            where: {
+                organization: { id: In(orgIds) },
+                isArchive: false,
+            },
+            relations: ['organization', 'user'], // <--- обязательно
+        });
 
-      return organizationReadDto;
-    } catch (err) {
-      this.logger.error(err);
-      if (err instanceof NotFoundException) {
-        throw err;
-      }
+        const postsMap = new Map<string, any[]>();
+        posts.forEach(post => {
+            // фильтруем только "корневые" посты
+            if (post.parentId === null) {
+                const orgId = post.organization.id;
 
-      throw new InternalServerErrorException(
-        'Ошибка при получении организации',
-      );
+                if (!postsMap.has(orgId)) postsMap.set(orgId, []);
+                postsMap.get(orgId)!.push({
+                    id: post.id,
+                    postName: post.postName,
+                    divisionName: post.divisionName,
+                    user: post.user,
+                    parentId: post.parentId,
+                });
+            }
+        });
+
+        return organizations.map(org => ({
+            ...org,
+            posts: postsMap.get(org.id) || [],
+        }));
     }
-  }
 
-  async create(organizationCreateDto: OrganizationCreateDto): Promise<string> {
-    try {
-      const organization = new Organization();
 
-      if (organizationCreateDto.id) organization.id = organizationCreateDto.id;
-
-      organization.organizationName = organizationCreateDto.organizationName;
-
-      if (organizationCreateDto.parentOrganizationId)
-        organization.parentOrganizationId =
-          organizationCreateDto.parentOrganizationId;
-
-      if (organizationCreateDto.reportDay)
-        organization.reportDay = organizationCreateDto.reportDay;
-
-      organization.organizationColor = organizationCreateDto.organizationColor;
-
-      organization.account = organizationCreateDto.account;
-
-      const createdOrganizationId =
-        await this.organizationRepository.insert(organization);
-      return createdOrganizationId.identifiers[0].id;
-    } catch (err) {
-      this.logger.error(err);
-
-      throw new InternalServerErrorException('Ошибка при создании организации');
+    async findAllForAccount(
+        accountId: string,
+        relations?: string[],
+    ): Promise<OrganizationReadDto[]> {
+        try {
+            const organizations = await this.organizationRepository.find({
+                where: {account: {id: accountId}},
+                relations: relations ?? [],
+            });
+            return organizations.map((organization) => ({
+                id: organization.id,
+                organizationName: organization.organizationName,
+                parentOrganizationId: organization.parentOrganizationId,
+                reportDay: organization.reportDay,
+                colorCodes: organization.colorCodes,
+                organizationColor: organization.organizationColor,
+                createdAt: organization.createdAt,
+                updatedAt: organization.updatedAt,
+                users: organization.users,
+                posts: organization.posts,
+                goal: organization.goal,
+                policies: organization.policies,
+                projects: organization.projects,
+                strategies: organization.strategies,
+                controlPanels: organization.controlPanels,
+                account: organization.account,
+            }));
+        } catch (err) {
+            this.logger.error(err);
+            throw new InternalServerErrorException(
+                'Ошибка при получении всех организаций!',
+            );
+        }
     }
-  }
 
-  async update(
-    _id: string,
-    updateOrganizationDto: OrganizationUpdateDto,
-  ): Promise<string> {
-    try {
-      const organization = await this.organizationRepository.findOne({
-        where: { id: _id },
-      });
-      if (!organization) {
-        throw new NotFoundException(`Организация с ID ${_id} не найдена`);
-      }
-      if (updateOrganizationDto.organizationName)
-        organization.organizationName = updateOrganizationDto.organizationName;
-      if (updateOrganizationDto.parentOrganizationId)
-        organization.parentOrganizationId =
-          updateOrganizationDto.parentOrganizationId;
-      if (updateOrganizationDto.reportDay !== undefined)
-        organization.reportDay = updateOrganizationDto.reportDay;
-      if (updateOrganizationDto.colorCodes)
-        organization.colorCodes = updateOrganizationDto.colorCodes;
-      if (updateOrganizationDto.organizationColor)
-        organization.organizationColor =
-          updateOrganizationDto.organizationColor;
-      await this.organizationRepository.update(_id, {
-        organizationName: organization.organizationName,
-        parentOrganizationId: organization.parentOrganizationId,
-        reportDay: organization.reportDay,
-        colorCodes: organization.colorCodes,
-        organizationColor: organization.organizationColor,
-      });
-      return organization.id;
-    } catch (err) {
-      this.logger.error(err);
-      if (err instanceof NotFoundException) {
-        throw err;
-      }
+    async findOneById(
+        id: string,
+        relations?: string[],
+    ): Promise<OrganizationReadDto> {
+        try {
+            const organization = await this.organizationRepository.findOne({
+                where: {id: id},
+                relations: relations ?? [],
+            });
 
-      throw new InternalServerErrorException(
-        'Ошибка при обновлении организации',
-      );
+            if (!organization)
+                throw new NotFoundException(`Организация с ID: ${id} не найдена`);
+            const organizationReadDto: OrganizationReadDto = {
+                id: organization.id,
+                organizationName: organization.organizationName,
+                parentOrganizationId: organization.parentOrganizationId,
+                reportDay: organization.reportDay,
+                colorCodes: organization.colorCodes,
+                organizationColor: organization.organizationColor,
+                createdAt: organization.createdAt,
+                updatedAt: organization.updatedAt,
+                users: organization.users,
+                posts: organization.posts,
+                goal: organization.goal,
+                policies: organization.policies,
+                projects: organization.projects,
+                strategies: organization.strategies,
+                controlPanels: organization.controlPanels,
+                account: organization.account,
+            };
+
+            return organizationReadDto;
+        } catch (err) {
+            this.logger.error(err);
+            if (err instanceof NotFoundException) {
+                throw err;
+            }
+
+            throw new InternalServerErrorException(
+                'Ошибка при получении организации',
+            );
+        }
     }
-  }
+
+    async create(organizationCreateDto: OrganizationCreateDto): Promise<string> {
+        try {
+            const organization = new Organization();
+
+            if (organizationCreateDto.id) organization.id = organizationCreateDto.id;
+
+            organization.organizationName = organizationCreateDto.organizationName;
+
+            if (organizationCreateDto.parentOrganizationId)
+                organization.parentOrganizationId =
+                    organizationCreateDto.parentOrganizationId;
+
+            if (organizationCreateDto.reportDay)
+                organization.reportDay = organizationCreateDto.reportDay;
+
+            organization.organizationColor = organizationCreateDto.organizationColor;
+
+            organization.account = organizationCreateDto.account;
+
+            const createdOrganizationId =
+                await this.organizationRepository.insert(organization);
+            return createdOrganizationId.identifiers[0].id;
+        } catch (err) {
+            this.logger.error(err);
+
+            throw new InternalServerErrorException('Ошибка при создании организации');
+        }
+    }
+
+    async update(
+        _id: string,
+        updateOrganizationDto: OrganizationUpdateDto,
+    ): Promise<string> {
+        try {
+            const organization = await this.organizationRepository.findOne({
+                where: {id: _id},
+            });
+            if (!organization) {
+                throw new NotFoundException(`Организация с ID ${_id} не найдена`);
+            }
+            if (updateOrganizationDto.organizationName)
+                organization.organizationName = updateOrganizationDto.organizationName;
+            if (updateOrganizationDto.parentOrganizationId)
+                organization.parentOrganizationId =
+                    updateOrganizationDto.parentOrganizationId;
+            if (updateOrganizationDto.reportDay !== undefined)
+                organization.reportDay = updateOrganizationDto.reportDay;
+            if (updateOrganizationDto.colorCodes)
+                organization.colorCodes = updateOrganizationDto.colorCodes;
+            if (updateOrganizationDto.organizationColor)
+                organization.organizationColor =
+                    updateOrganizationDto.organizationColor;
+            await this.organizationRepository.update(_id, {
+                organizationName: organization.organizationName,
+                parentOrganizationId: organization.parentOrganizationId,
+                reportDay: organization.reportDay,
+                colorCodes: organization.colorCodes,
+                organizationColor: organization.organizationColor,
+            });
+            return organization.id;
+        } catch (err) {
+            this.logger.error(err);
+            if (err instanceof NotFoundException) {
+                throw err;
+            }
+
+            throw new InternalServerErrorException(
+                'Ошибка при обновлении организации',
+            );
+        }
+    }
 }
--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,171 +1,195 @@
 import {
-  Body,
-  Controller,
-  Get,
-  HttpStatus,
-  Inject,
-  Param,
-  Patch,
-  Post,
-  Req,
-  UseGuards,
+    Body,
+    Controller,
+    Get,
+    HttpStatus,
+    Inject,
+    Param,
+    Patch,
+    Post,
+    Req,
+    UseGuards,
 } from '@nestjs/common';
-import { OrganizationService } from 'src/application/services/organization/organization.service';
-import { OrganizationCreateDto } from 'src/contracts/organization/create-organization.dto';
-import { OrganizationReadDto } from 'src/contracts/organization/read-organization.dto';
+import {OrganizationService} from 'src/application/services/organization/organization.service';
+import {OrganizationCreateDto} from 'src/contracts/organization/create-organization.dto';
+import {OrganizationReadDto} from 'src/contracts/organization/read-organization.dto';
 
 import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiParam,
-  ApiResponse,
-  ApiTags,
+    ApiBearerAuth,
+    ApiOperation,
+    ApiParam,
+    ApiResponse,
+    ApiTags,
 } from '@nestjs/swagger';
-import { OrganizationUpdateDto } from 'src/contracts/organization/update-organization.dto';
-import { Logger } from 'winston';
-import { yellow } from 'colorette';
-import { AccessTokenGuard } from 'src/guards/accessToken.guard';
-import { Request as ExpressRequest } from 'express';
-import { ReadUserDto } from 'src/contracts/user/read-user.dto';
+import {OrganizationUpdateDto} from 'src/contracts/organization/update-organization.dto';
+import {Logger} from 'winston';
+import {yellow} from 'colorette';
+import {AccessTokenGuard} from 'src/guards/accessToken.guard';
+import {Request as ExpressRequest} from 'express';
+import {ReadUserDto} from 'src/contracts/user/read-user.dto';
 import {
-  findAllOrganizationsExample,
-  findOneOrganizationExample,
+    findAllOrganizationsExample,
+    findOneOrganizationExample,
 } from 'src/constants/swagger-examples/organization/organization-example';
 
 @UseGuards(AccessTokenGuard)
 @ApiTags('Organization')
 @ApiBearerAuth('access-token')
 @Controller('organizations')
+
 export class OrganizationController {
-  constructor(
-    private readonly organizationService: OrganizationService,
-    @Inject('winston') private readonly logger: Logger,
-  ) {}
+    constructor(
+        private readonly organizationService: OrganizationService,
+        @Inject('winston') private readonly logger: Logger,
+    ) {
+    }
 
-  @Get()
-  @ApiOperation({ summary: 'Все организации в аккаунте' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'ОК!',
-    example: findAllOrganizationsExample,
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: 'Вы не авторизованы!',
-  })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Ошибка сервера!',
-  })
-  async findAll(@Req() req: ExpressRequest): Promise<OrganizationReadDto[]> {
-    const user = req.user as ReadUserDto;
-    return await this.organizationService.findAllForAccount(user.account.id);
-  }
+    @Get()
+    @ApiOperation({summary: 'Все организации в аккаунте'})
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'ОК!',
+        example: findAllOrganizationsExample,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: 'Вы не авторизованы!',
+    })
+    @ApiResponse({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        description: 'Ошибка сервера!',
+    })
+    async findAll(@Req() req: ExpressRequest): Promise<OrganizationReadDto[]> {
+        const user = req.user as ReadUserDto;
+        return await this.organizationService.findAllForAccount(user.account.id);
+    }
 
-  @Patch(':organizationId/update')
-  @ApiOperation({ summary: 'Обновить данные об организации по ID' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'ОК!',
-    example: { id: '2d1cea4c-7cea-4811-8cd5-078da7f20167' },
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: 'Вы не авторизованы!',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Ошибка валидации!',
-  })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Ошибка сервера!',
-  })
-  @ApiParam({
-    name: 'organizationId',
-    required: true,
-    description: 'Id организации',
-    example: '2d1cea4c-7cea-4811-8cd5-078da7f20167',
-  })
-  async update(
-    @Param('organizationId') organizationId: string,
-    @Body() organizationUpdateDto: OrganizationUpdateDto,
-  ): Promise<{ id: string }> {
-    const updatedOrganizationId = await this.organizationService.update(
-      organizationId,
-      organizationUpdateDto,
-    );
-    this.logger.info(
-      `${yellow('OK!')} - UPDATED ORGANIZATION: ${JSON.stringify(organizationUpdateDto)} - Организация успешно обновлена!`,
-    );
-    return { id: updatedOrganizationId };
-  }
+    @Get('highPost')
+    @ApiOperation({summary: 'Все организации в аккаунте (highPost?)'})
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'ОК!',
+        example: findAllOrganizationsExample,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: 'Вы не авторизованы!',
+    })
+    @ApiResponse({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        description: 'Ошибка сервера!',
+    })
+    async findAllHighPost(
+        @Req() req: ExpressRequest,
+    ): Promise<OrganizationReadDto[]> {
+        const user = req.user as ReadUserDto;
+        return await this.organizationService.findAllWithHighPostForAccount(user.account.id);
+    }
 
-  @Post('new')
-  @ApiOperation({ summary: 'Добавить организацию в аккаунт' })
-  @ApiResponse({
-    status: HttpStatus.CREATED,
-    description: 'CREATED!',
-    example: { id: '2d1cea4c-7cea-4811-8cd5-078da7f20167' },
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Ошибка валидации!',
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: 'Вы не авторизованы!',
-  })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Ошибка сервера!',
-  })
-  @ApiParam({
-    name: 'userId',
-    required: true,
-    description: 'Id пользователя',
-    example: 'bc807845-08a8-423e-9976-4f60df183ae2',
-  })
-  async create(
-    @Req() req: ExpressRequest,
-    @Body() organizationCreateDto: OrganizationCreateDto,
-  ): Promise<{ id: string }> {
-    const user = req.user as ReadUserDto;
-    organizationCreateDto.account = user.account;
-    const createdOrganizationId = await this.organizationService.create(
-      organizationCreateDto,
-    );
-    return { id: createdOrganizationId };
-  }
+    @Patch(':organizationId/update')
+    @ApiOperation({summary: 'Обновить данные об организации по ID'})
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'ОК!',
+        example: {id: '2d1cea4c-7cea-4811-8cd5-078da7f20167'},
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: 'Вы не авторизованы!',
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description: 'Ошибка валидации!',
+    })
+    @ApiResponse({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        description: 'Ошибка сервера!',
+    })
+    @ApiParam({
+        name: 'organizationId',
+        required: true,
+        description: 'Id организации',
+        example: '2d1cea4c-7cea-4811-8cd5-078da7f20167',
+    })
+    async update(
+        @Param('organizationId') organizationId: string,
+        @Body() organizationUpdateDto: OrganizationUpdateDto,
+    ): Promise<{ id: string }> {
+        const updatedOrganizationId = await this.organizationService.update(
+            organizationId,
+            organizationUpdateDto,
+        );
+        this.logger.info(
+            `${yellow('OK!')} - UPDATED ORGANIZATION: ${JSON.stringify(organizationUpdateDto)} - Организация успешно обновлена!`,
+        );
+        return {id: updatedOrganizationId};
+    }
 
-  @Get(':organizationId')
-  @ApiOperation({ summary: 'Получить организацию по id' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'ОК!',
-    example: findOneOrganizationExample,
-  })
-  @ApiResponse({
-    status: HttpStatus.UNAUTHORIZED,
-    description: 'Вы не авторизованы!',
-  })
-  @ApiResponse({
-    status: HttpStatus.NOT_FOUND,
-    description: 'Организация не найдена',
-  })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Ошибка сервера!',
-  })
-  @ApiParam({
-    name: 'organizationId',
-    required: true,
-    description: 'Id организации',
-    example: '2d1cea4c-7cea-4811-8cd5-078da7f20167',
-  })
-  async findOne(
-    @Param('organizationId') organizationId: string,
-  ): Promise<OrganizationReadDto> {
-    return await this.organizationService.findOneById(organizationId, ['users.posts']);
-  }
+    @Post('new')
+    @ApiOperation({summary: 'Добавить организацию в аккаунт'})
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: 'CREATED!',
+        example: {id: '2d1cea4c-7cea-4811-8cd5-078da7f20167'},
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description: 'Ошибка валидации!',
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: 'Вы не авторизованы!',
+    })
+    @ApiResponse({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        description: 'Ошибка сервера!',
+    })
+    @ApiParam({
+        name: 'userId',
+        required: true,
+        description: 'Id пользователя',
+        example: 'bc807845-08a8-423e-9976-4f60df183ae2',
+    })
+    async create(
+        @Req() req: ExpressRequest,
+        @Body() organizationCreateDto: OrganizationCreateDto,
+    ): Promise<{ id: string }> {
+        const user = req.user as ReadUserDto;
+        organizationCreateDto.account = user.account;
+        const createdOrganizationId = await this.organizationService.create(
+            organizationCreateDto,
+        );
+        return {id: createdOrganizationId};
+    }
+
+    @Get(':organizationId')
+    @ApiOperation({summary: 'Получить организацию по id'})
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: 'ОК!',
+        example: findOneOrganizationExample,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: 'Вы не авторизованы!',
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: 'Организация не найдена',
+    })
+    @ApiResponse({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        description: 'Ошибка сервера!',
+    })
+    @ApiParam({
+        name: 'organizationId',
+        required: true,
+        description: 'Id организации',
+        example: '2d1cea4c-7cea-4811-8cd5-078da7f20167',
+    })
+    async findOne(
+        @Param('organizationId') organizationId: string,
+    ): Promise<OrganizationReadDto> {
+        return await this.organizationService.findOneById(organizationId, ['users.posts']);
+    }
 }
